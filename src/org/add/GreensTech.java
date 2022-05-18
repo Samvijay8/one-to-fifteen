3b6da2b5c280446f80b75d456e438cdc14d0b970
samvijay8 <samvijay8@gmail.com>
2022-05-18 12:11:02 +0530

    initial commit

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+private void greensOmr() {
+	// TODO Auto-generated method stub
+System.out.println("Greens Omr");
+}
+private void greensAdayar() {
+	// TODO Auto-generated method stub
+System.out.println("Greens Adayar");
+}
+private void greensTambaram() {
+	// TODO Auto-generated method stub
+System.out.println("Greens Velachery");
+}
+private void greensAnnanagar() {
+	// TODO Auto-generated method stub
+System.out.println("Greens Annanagar");
+}
+public static void main(String[] args) {
+	GreensTech g=new GreensTech();
+	g.greensOmr();
+	g.greensAdayar();
+	g.greensTambaram();
+	g.greensAnnanagar();
+}
+}
---
+++ src/org/add1/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add1;
+
+public class GreensTech {
+	
+	private void greensOmr(int id) {
+		System.out.println(id);
+	}
+	
+	private void greensOmr(String name) {
+		System.out.println(name);
+	}
+	
+	private void greensOmr(long num) {
+		System.out.println(num);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		GreensTech g=new GreensTech();
+		g.greensOmr(214);
+		g.greensOmr("Greens Technology Pvt Ltd.");
+		g.greensOmr(6383468626l);
+	}
+
+}
